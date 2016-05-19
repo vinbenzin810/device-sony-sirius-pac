494faff42e4ea653f9d57e1c625fe1a87c50efdf
Max Weffers <rcstar6696@gmail.com>
2016-05-19 16:16:18 +0200

    Bring up PAC-ROM Makefiles

---
+++ pac.mk
@@ -0,0 +1,37 @@
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/sony/msm
+TARGET_KERNEL_CONFIG := aosp_shinano_sirius_defconfig
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/sony/sirius/device.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common PAC stuff.
+$(call inherit-product, vendor/pac/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Boot animation
+PAC_BOOTANIMATION_NAME := 1080
+
+PRODUCT_NAME := pac_sirius
+PRODUCT_DEVICE := sirius
+PRODUCT_MODEL := Xperia Z2 (AOSP)
+PRODUCT_BRAND := Sony
+PRODUCT_MANUFACTURER := Sony
